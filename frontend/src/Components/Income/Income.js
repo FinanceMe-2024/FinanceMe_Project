da337ddfe5a7f855ffd3d5876c9def2fd4f56e9b
sdcs23 <apolocubes@gmail.com>
2024-03-06 22:01:52 -0500

    Income Historial

--- frontend/src/Components/Income/Income.js
+++ frontend/src/Components/Income/Income.js
@@ -3,6 +3,7 @@ import styled from 'styled-components'
 import { useGlobalContext } from '../../context/globalContext';
 import { InnerLayout } from '../../styles/Layouts';
 import Form from '../Form/Form';
+import IncomeItem from '../IncomeItem/IncomeItem';
 
 function Income() {
     const {addIncome,incomes, getIncomes, deleteIncome, totalIncome} = useGlobalContext()
@@ -18,6 +19,23 @@ function Income() {
                 <div className="income-content">
                     <div className="form-container">
                         <Form />
+                    </div>
+                    <div className="incomes">
+                        {incomes.map((income) => {
+                            const {_id, title, amount, date, category, description, type} = income;
+                            return <IncomeItem
+                                key={_id}
+                                id={_id} 
+                                title={title} 
+                                description={description} 
+                                amount={amount} 
+                                date={date} 
+                                type={type}
+                                category={category} 
+                                indicatorColor="var(--color-green)"
+                                deleteItem={deleteIncome}
+                            />
+                        })}
                     </div>
                 </div>
             </InnerLayout>
