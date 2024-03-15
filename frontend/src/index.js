7da2130fb0752278e0258affe8d4e066ddc9772b
aliss-55 <alisson.tobar@uao.edu.co>
2024-03-15 17:21:14 -0500

    Configuracion despliegue frontend

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,7 +5,7 @@ import { GlobalProvider } from './context/globalContext';
 import { GlobalStyle } from './styles/GlobalStyle';
 import { disableReactDevTools } from  '@fvilers/disable-react-devtools'
 
-if (process.env.NODE_ENV === 'production') disableReactDevTools;
+if (process.env.NODE_ENV === 'production') {   disableReactDevTools(); }
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
