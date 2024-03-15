74e0e18e320ff7ce9dd28682980fbe37b449a57d
aliss-55 <alisson.tobar@uao.edu.co>
2024-03-15 15:24:30 -0500

    Configuracion despliegue frontend

--- frontend/src/context/globalContext.js
+++ frontend/src/context/globalContext.js
@@ -2,7 +2,7 @@ import React, { useContext, useState } from "react"
 import axios from 'axios'
 
 
-const BASE_URL = "http://localhost:5050/api/v1/";
+const BASE_URL = "http://financeme/api/v1/.com";
 
 
 const GlobalContext = React.createContext()
