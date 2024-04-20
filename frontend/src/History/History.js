569d398e982f0c5bb73cce3764fcfbb59763861f
alis-55 <alisson.tobar@uao.edu.co>
2024-04-19 23:49:25 -0500

    original

--- frontend/src/History/History.js
+++ frontend/src/History/History.js
@@ -1,40 +1,53 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from 'react'
+import styled from 'styled-components'
 import { useGlobalContext } from '../context/globalContext';
 
 function History() {
-    const { transactionHistory } = useGlobalContext();
-    const history = transactionHistory();
+    const {transactionHistory} = useGlobalContext()
+
+    const [...history] = transactionHistory()
 
     return (
         <HistoryStyled>
             <h2>Recent History</h2>
-            {history.map((item) => {
-                const { _id, title, amount, type } = item;
-                const sign = type === 'expense' ? '-' : '+';
-                const color = type === 'expense' ? 'red' : 'var(--color-green)';
-
+            {history.map((item) =>{
+                const {_id, title, amount, type} = item
                 return (
                     <div key={_id} className="history-item">
-                        <p style={{ color }}>{title}</p>
-                        <p style={{ color }}>{`${sign}${Math.abs(amount).toFixed(2)}`}</p>
+                        <p style={{
+                            color: type === 'expense' ? 'red' : 'var(--color-green)'
+                        }}>
+                            {title}
+                        </p>
+
+                        <p style={{
+                            color: type === 'expense' ? 'red' : 'var(--color-green)'
+                        }}>
+                            {
+                                type === 'expense' ? `-${amount <= 0 ? 0 : amount}` : `+${amount <= 0 ? 0: amount}`
+                            }
+                        </p>
                     </div>
-                );
+                )
             })}
         </HistoryStyled>
-    );
+    )
 }
 
 const HistoryStyled = styled.div`
-    h2 {
-        margin-bottom: 20px;
-    }
-
-    .history-item {
+    display: flex;
+    flex-direction: column;
+    gap: 1rem;
+    .history-item{
+        background: #FCF6F9;
+        border: 2px solid #FFFFFF;
+        box-shadow: 0px 1px 15px rgba(0, 0, 0, 0.06);
+        padding: 1rem;
+        border-radius: 20px;
         display: flex;
         justify-content: space-between;
-        margin-bottom: 10px;
+        align-items: center;
     }
 `;
 
-export default History;
+export default History
