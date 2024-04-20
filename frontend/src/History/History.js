7e3fde6568c2a7766ea35710a66159c4ad748c15
alis-55 <alisson.tobar@uao.edu.co>
2024-04-20 09:36:01 -0500

    Hisotry colors

--- frontend/src/History/History.js
+++ frontend/src/History/History.js
@@ -1,37 +1,29 @@
-import React from 'react'
-import styled from 'styled-components'
+import React from 'react';
+import styled from 'styled-components';
 import { useGlobalContext } from '../context/globalContext';
 
 function History() {
-    const {transactionHistory} = useGlobalContext()
-
-    const [...history] = transactionHistory()
+    const { transactionHistory } = useGlobalContext();
+    const history = transactionHistory();
 
     return (
         <HistoryStyled>
             <h2>Recent History</h2>
-            {history.map((item) =>{
-                const {_id, title, amount, type} = item
+            {history.map((item) => {
+                const { _id, title, amount, type } = item;
+                const amountValue = Math.abs(amount).toFixed(2); // Valor absoluto del monto con 2 decimales
+                const sign = type === 'income' ? '+' : '-'; // Signo '-' para gastos (expense) y '+' para ingresos (income)
+                const color = type === 'income' ? 'green' : 'red'; // Color rojo para gastos y verde para ingresos
+                
                 return (
                     <div key={_id} className="history-item">
-                        <p style={{
-                            color: type === 'expense' ? 'red' : 'var(--color-green)'
-                        }}>
-                            {title}
-                        </p>
-
-                        <p style={{
-                            color: type === 'expense' ? 'red' : 'var(--color-green)'
-                        }}>
-                            {
-                                type === 'expense' ? `-${amount <= 0 ? 0 : amount}` : `+${amount <= 0 ? 0: amount}`
-                            }
-                        </p>
+                        <p style={{ color }}>{title}</p>
+                        <p style={{ color }}>{`${sign}${amountValue}`}</p>
                     </div>
-                )
+                );
             })}
         </HistoryStyled>
-    )
+    );
 }
 
 const HistoryStyled = styled.div`
