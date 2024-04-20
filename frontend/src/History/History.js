e3726239762271a8d27f6f88cc6648a58d66e299
alis-55 <alisson.tobar@uao.edu.co>
2024-04-19 23:39:28 -0500

    history 2.0

--- frontend/src/History/History.js
+++ frontend/src/History/History.js
@@ -15,13 +15,13 @@ function History() {
                 return (
                     <div key={_id} className="history-item">
                         <p style={{
-                            color: type === 'income' ? 'green' : 'var(--color-green)'
+                            color: type === 'expense' ? 'red' : 'var(--color-green)'
                         }}>
                             {title}
                         </p>
 
                         <p style={{
-                            color: type === 'expense' ? 'red' : 'var(--color-red)'
+                            color: type === 'expense' ? 'red' : 'var(--color-reed)'
                         }}>
                             {
                                 type === 'expense' ? `-${amount <= 0 ? 0 : amount}` : `+${amount <= 0 ? 0: amount}`
